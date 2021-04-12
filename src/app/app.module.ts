781720d0d2f1dc3708c66bb39ddfc8f9f4ccb0b5
Steven Lazo <enriquelazo_11@hotmail.com>
2021-04-11 19:33:48 -0500

    Codigo Qr

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { NgxQRCodeModule } from '@techiediaries/ngx-qrcode';
+
+import { FormsModule } from '@angular/forms';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    NgxQRCodeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
